938345dc10e009675630741f43cc5059376c3b6c
Mauricio Tur <mauri@MacBook-Air-de-Mauricio.local>
2026-06-27 17:27:18 +0200

    feat: remove QR download buttons from machine detail screen

--- app/test/screens/machine_detail_screen_test.dart
+++ app/test/screens/machine_detail_screen_test.dart
@@ -1,4 +1,3 @@
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -24,28 +23,12 @@ void main() {
     when(() => api.getMachineById('machine-1')).thenAnswer((_) async => testMachine);
   });
 
-  testWidgets('shows PNG and PDF download buttons', (tester) async {
+  testWidgets('displays machine QR code', (tester) async {
     await tester.pumpWidget(MaterialApp(
       home: MachineDetailScreen(api: api, machineId: 'machine-1'),
     ));
     await tester.pumpAndSettle();
 
-    expect(find.text('PNG'), findsOneWidget);
-    expect(find.text('PDF'), findsOneWidget);
-  });
-
-  testWidgets('tapping PDF button calls getMachineQrPdf', (tester) async {
-    when(() => api.getMachineQrPdf('machine-1'))
-        .thenAnswer((_) async => Uint8List(0));
-
-    await tester.pumpWidget(MaterialApp(
-      home: MachineDetailScreen(api: api, machineId: 'machine-1'),
-    ));
-    await tester.pumpAndSettle();
-
-    await tester.tap(find.text('PDF'));
-    await tester.pumpAndSettle();
-
-    verify(() => api.getMachineQrPdf('machine-1')).called(1);
+    expect(find.text(testMachine.qrCode), findsWidgets);
   });
 }
